4411c02ede377894765eed8e57dc57838e35c8eb
CarlH2019 <hofstromcarl@gmail.com>
2019-07-19 19:44:21 +0000

    after lesson 28

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,19 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson
 
   def show
   end
 
 private
 
-  helper_method :current_lesson
+def require_authorized_for_current_lesson
+  if !current_user.enrolled_in?(current_lesson.section.course)  # the enrolled_in? method takes an argument 
+      redirect_to course_path(current_lesson.section.course), alert: 'You must enroll first'   # the url path may be course_path instead of courses_path
+    end
+  end
+
+    helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
